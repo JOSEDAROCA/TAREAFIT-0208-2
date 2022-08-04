ecc71c834f6598b852a7e75974c403fa78945366
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-08-04 12:53:58 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>NUEVO EJERCICIO PARA FIT 
+<DIV>
+FULL STACK BENITO JUAREZ</h1>
+<div>
+- Linea 1 de Texto
+<div>
+- Linea 2 de Texto
+<div>
+- Linea 3 de Texto
+
+</div>`;
